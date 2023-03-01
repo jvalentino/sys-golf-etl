26b6e271f41acc3725f0130397e08c4f3605f5d1
John Valentino <jvalentino2@gmail.com>
2023-03-01 06:47:23 -0600

    general structure

--- build-docker.sh
+++ build-docker.sh
@@ -2,8 +2,11 @@
 
 NAME=sys-golf-etl
 VERSION=latest
+HELM_NAME=etl
 
+helm delete $HELM_NAME || true
 minikube image rm $NAME:$VERSION
+rm -rf ~/.minikube/cache/images/arm64/$NAME_$VERSION || true
 docker build --no-cache . -t $NAME
 minikube image load $NAME:$VERSION
 # minikube cache reload
