611c0192a28b94fc986a66b16656b908fb1b6146
John Valentino <jvalentino2@gmail.com>
2023-03-03 08:39:07 -0600

    Added prometheus to ETL

--- src/main/groovy/com/github/jvalentino/golf/dw/config/SpringWebConfig.groovy
+++ src/main/groovy/com/github/jvalentino/golf/dw/config/SpringWebConfig.groovy
@@ -6,7 +6,9 @@ import groovy.util.logging.Slf4j
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.http.MediaType
 import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -30,6 +32,13 @@ class SpringWebConfig implements WebMvcConfigurer {
     @Override
     void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new MappingJackson2HttpMessageConverter(jsonMapper()))
+
+        // requires for prometheus endpoint
+        StringHttpMessageConverter converter = new StringHttpMessageConverter()
+        converter.setSupportedMediaTypes(Arrays.asList(
+                MediaType.TEXT_PLAIN,
+                new MediaType('application', 'openmetrics-text')))
+        converters.add(converter)
     }
 
 }
